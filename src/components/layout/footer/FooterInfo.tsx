a49ccf45271f1c59654335260db415213a998024
oimasterkafuu <oimasterfake@icloud.com>
2024-07-18 21:48:30 +0800

    chore: update footer

--- src/components/layout/footer/FooterInfo.tsx
+++ src/components/layout/footer/FooterInfo.tsx
@@ -201,7 +201,7 @@ const FooterBottom = async () => {
           </SubscribeTextButton>
         </span>
         <span className="mt-3 block md:mt-0 md:inline">
-          Stay hungry. Stay foolish.
+          Salve ad meum blogum!
         </span>
       </p>
       <div>
